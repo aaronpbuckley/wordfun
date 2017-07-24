33053a853cb7deb127f771f190c07b9f17dea46b
Aaron Buckley <apb.buckley@gmail.com>
2017-07-24 16:45:51 -0700

    Initial Commit

---
+++ word fun/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  word fun
+//
+//  Created by Aaron Buckley on 7/24/17.
+//  Copyright © 2017 Aaron Buckley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
