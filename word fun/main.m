43df2920e7f22ea1f3e7b8e1de0d2f6174b7151b
Aaron Buckley <apb.buckley@gmail.com>
2017-07-24 17:45:35 -0700

    word fun before func

--- word fun/main.m
+++ word fun/main.m
@@ -10,8 +10,27 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        char inputChars[255];
+        printf("input a string: ");
+        fgets(inputChars, 255, stdin);
+        printf("your string is %s\n", inputChars);
+        NSString *inputString = [NSString stringWithUTF8String:inputChars];
+        NSLog(@"input was: %@", inputString);
+        
+        NSLog(@"Uppercase: %@",[inputString uppercaseString]);
+        NSLog(@"Lowercase: %@",[inputString lowercaseString]);
+        NSLog(@"Convert to number(?) %d",[inputString intValue]);
+        NSLog(@"Canadianize: %@",[inputString stringByAppendingString:@", eh?"]);
+        if ([inputString hasSuffix:@"!"])
+        {
+            NSLog(@"Woah, calm down!");
+        }
+        else if ([inputString hasSuffix:@"?"])
+        {
+            NSLog(@"I don't know");
+        }
+        else NSLog(@"%@", inputString);
+        NSLog(@"%@",[inputString stringByReplacingOccurancesOfString:@" " withString:@"-"]);
     }
     return 0;
 }
