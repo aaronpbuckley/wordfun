6ed11e0452ed05d6bd85b8f447267376617f6c85
Aaron Buckley <apb.buckley@gmail.com>
2017-07-24 18:23:00 -0700

    added while loop

--- word fun/main.m
+++ word fun/main.m
@@ -10,27 +10,30 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        char inputChars[255];
-        printf("input a string: ");
-        fgets(inputChars, 255, stdin);
-        printf("your string is %s\n", inputChars);
-        NSString *inputString = [NSString stringWithUTF8String:inputChars];
-        NSLog(@"input was: %@", inputString);
-        
-        NSLog(@"Uppercase: %@",[inputString uppercaseString]);
-        NSLog(@"Lowercase: %@",[inputString lowercaseString]);
-        NSLog(@"Convert to number(?) %d",[inputString intValue]);
-        NSLog(@"Canadianize: %@",[inputString stringByAppendingString:@", eh?"]);
-        if ([inputString hasSuffix:@"!"])
-        {
-            NSLog(@"Woah, calm down!");
+        while (true) {
+            printf("uppercase, lowercase, canadianize \n");
+            char inputChars[255];
+            printf("input a string: ");
+            fgets(inputChars, 255, stdin);
+            printf("your string is %s\n", inputChars);
+            NSString *inputString = [NSString stringWithUTF8String:inputChars];
+            NSLog(@"input was: %@", inputString);
+            
+            NSLog(@"Uppercase: %@",[inputString uppercaseString]);
+            NSLog(@"Lowercase: %@",[inputString lowercaseString]);
+            NSLog(@"Convert to number(?) %d",[inputString intValue]);
+            NSLog(@"Canadianize: %@",[inputString stringByAppendingString:@", eh?"]);
+            if ([inputString hasSuffix:@"!"])
+            {
+                NSLog(@"Woah, calm down!");
+            }
+            else if ([inputString hasSuffix:@"?"])
+            {
+                NSLog(@"I don't know");
+            }
+            else NSLog(@"%@", inputString);
+      
         }
-        else if ([inputString hasSuffix:@"?"])
-        {
-            NSLog(@"I don't know");
-        }
-        else NSLog(@"%@", inputString);
-        NSLog(@"%@",[inputString stringByReplacingOccurancesOfString:@" " withString:@"-"]);
     }
     return 0;
 }
